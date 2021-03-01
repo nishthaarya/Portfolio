8298c292d17c6e3dba6596df465fae9014fdd61f
Nishtha Arya <aryanishthaa@gmail.com>
2021-03-02 05:26:58 +0530

    About me page + stats

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import styles from "./Home.module.css";
 import Parallax from "react-rellax";
 import {motion} from "framer-motion";
-import pic from "./pic.jpeg"
-import pics from "./pic-removebg-preview.png";
 import Particles from 'react-particles-js';
 import ScrollAnimation from 'react-animate-on-scroll';
-import netflix1 from "./netflix1.png"
-import netflix2 from "./netflix2.png"
-import laptop from "./laptop.png"
+import redux from "./redux.png";
+import cart from "./cart.png";
+import netflix from "./netflix.png";
+import expense from "./expense.png";
+import self from "./pic.png";
+import youtube from "./youtube.png";
+import github from "./github.png"
 
 export const Home = () => {
 
@@ -53,7 +55,7 @@ export const Home = () => {
                 animate = {{y: 250}}
                 transition = {{delay: 2, duration: 1, type: "spring", stiffness: 100}}
             >
-                Full Stack Web Developer
+                Frontend Web Developer.
             </motion.div>
             <motion.button
                 className = {styles.btn}
@@ -112,8 +114,7 @@ export const Home = () => {
             >
             </Particles>
         </div>
-        <Parallax speed = {7}>
-        <ScrollAnimation duration = {2} animateIn="fadeIn">
+        <Parallax speed = {5}>
         <div className = {styles.aboutpage}>
                 <div className = {styles.tech}>Tech Stack</div>
                 <div className = {styles.abtbtm}>
@@ -134,7 +135,7 @@ export const Home = () => {
                         <div> React </div>
                     </div>
                     <div className = {styles.box}>
-                        <img className = {styles.img} src = "http://assets.stickpng.com/images/5848309bcef1014c0b5e4a9a.png" alt = "redux" />
+                        <img className = {styles.img} src = {redux} alt = "redux" />
                         <div> Redux </div>
                     </div>
                     <div className = {styles.box}>
@@ -147,28 +148,144 @@ export const Home = () => {
                     </div>
                 </div>
             </div>
-        </ScrollAnimation>
         </Parallax>
         <Parallax speed = {5}>
             <div className = {styles.projects}>
                 <div className = {styles.tech}> Projects </div>
                 <div className = {styles.card}>
                     <div className = {styles.cardleft}>
-                        <img alt = "netflix1" src = "https://o.remove.bg/downloads/0e2dc311-6994-454f-bea8-ca9080ea5ed7/A156D6E0-8D3E-4C3A-9037-00CAE30415BE-removebg-preview.png" />
+                        <img alt = "netflix1" src = {netflix} />
                         
                     </div>
                     <div className = {styles.cardright}>
                         <div className = {styles.heading}>Netflix Clone</div>
-                        <div className = {styles.extra}>
-                            <a href = "https://github.com/lsriniwas/Netflix-Clone" className = {styles.github}>Github Repository</a>
-                            <a href = "https://github.com/lsriniwas/Netflix-Clone" className = {styles.demo}> Demo Link </a>
+                        <div className = {styles.text}> Group Project | One Week </div>
+                        <div className = {styles.smallicons}>
+                            <img alt = "react" src = "https://cdn.svgporn.com/logos/react.svg"/>
+                            <img alt = "redux" src = {redux} />
+                            <img alt = "css" src = "https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png"/>
+                            <img alt = "mongodb" src = "https://banner2.cleanpng.com/20180403/hyq/kisspng-mongodb-node-js-npm-open-source-model-angularjs-leaf-5ac44d9ed28fc7.7809651415228143668625.jpg"/>
+                            <img alt = "nodejs" src = "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"/>
                         </div>
                         <div className = {styles.details}>
                             <div>Cloned the biggest online video streaming platform, Netflix,</div>
                             <div> with a team of 4 developers.</div>
                         </div>
+                        <div className = {styles.extra}>
+                            <a href = "https://github.com/lsriniwas/Netflix-Clone" className = {styles.github}>Github Repository</a>
+                            <a href = "https://masai-netflix.netlify.app/" className = {styles.demo}> Demo Link </a>
+                        </div>
                     </div>
                 </div>
+                <div className = {styles.card}>
+                    <div className = {styles.cardleft}>
+                        <img alt = "shoppingcart" src = {cart} />
+                        
+                    </div>
+                    <div className = {styles.cardright}>
+                        <div className = {styles.heading}>E-Commerce Website</div>
+                        <div className = {styles.text}> Solo Project | Two Weeks </div>
+                        <div className = {styles.smallicons}>
+                            <img alt = "react" src = "https://cdn.svgporn.com/logos/react.svg"/>
+                            <img alt = "redux" src = {redux} />
+                            <img alt = "css" src = "https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png"/>
+                            <img alt = "mongodb" src = "https://banner2.cleanpng.com/20180403/hyq/kisspng-mongodb-node-js-npm-open-source-model-angularjs-leaf-5ac44d9ed28fc7.7809651415228143668625.jpg"/>
+                            <img alt = "nodejs" src = "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"/>
+                        </div>
+                        <div className = {styles.details}>
+                            <div>Created a full stack e-commerce website from scratch.</div>
+                            <div> Seamless shopping experience with an intuitive UI.</div>
+                        </div>
+                        <div className = {styles.extra}>
+                            <a href = "https://github.com/nishthaarya/E-Commerce-Website" className = {styles.github}>Github Repository</a>
+                            <a href = "https://github.com/nishthaarya/E-Commerce-Website" className = {styles.demo}> Demo Link </a>
+                        </div>
+                    </div>
+                </div>
+                <div className = {styles.card}>
+                    <div className = {styles.cardleft}>
+                        <img alt = "expensemanager" src = {expense} />
+                        
+                    </div>
+                    <div className = {styles.cardright}>
+                        <div className = {styles.heading}>Expense Manager</div>
+                        <div className = {styles.text}> Pair Project | 3 days </div>
+                        <div className = {styles.smallicons}>
+                            <img alt = "javascript" src = "https://cdn.iconscout.com/icon/free/png-256/javascript-2752148-2284965.png"/>
+                            <img alt = "html" src = "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/html5-512.png" />
+                            <img alt = "css" src = "https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png"/>
+                        </div>
+                        <div className = {styles.details}>
+                            <div>Built an expense managing app that allows a user to </div>
+                            <div>add and view credit and debit transactions.</div>
+                        </div>
+                        <div className = {styles.extra}>
+                            <a href = "https://github.com/MahenParameshwar/ExpenceManager" className = {styles.github}>Github Repository</a>
+                            <a href = "https://expensemanagerjs.netlify.app/login.html" className = {styles.demo}> Demo Link </a>
+                        </div>
+                    </div>
+                </div>
+                <div className = {styles.card}>
+                    <div className = {styles.cardleft}>
+                        <img alt = "youtubeclone" src = {youtube} />
+                        
+                    </div>
+                    <div className = {styles.cardright}>
+                        <div className = {styles.heading}>Youtube Clone</div>
+                        <div className = {styles.text}> Group Project | 1 Week </div>
+                        <div className = {styles.smallicons}>
+                            <img alt = "react" src = "https://cdn.svgporn.com/logos/react.svg"/>
+                            <img alt = "css" src = "https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png"/>
+                        </div>
+                        <div className = {styles.details}>
+                            <div>Created a picture perfect clone of Youtube, implemented debouncing search feature</div>
+                            <div>along with recommendations being fetched from the Youtube API.</div>
+                        </div>
+                        <div className = {styles.extra}>
+                            <a href = "https://github.com/nishthaarya/youtube-clone-1" className = {styles.github}>Github Repository</a>
+                            <a href = "https://expensemanagerjs.netlify.app/login.html" className = {styles.demo}> Demo Link </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Parallax>
+        <Parallax speed = {5}>
+            <div className = {styles.projects1}>
+                <div className = {styles.circlespage}>
+                    <div className = {styles.circletop}>
+                        <div className = {styles.gol} > <div> 1200+ hours full stack coding</div> </div>
+                        <div className = {styles.gol}> <div>100+ hours soft skills training</div> </div>
+                        <div className = {styles.gol}><div> 300+ Hackerrank problems </div></div>
+                        <div className = {styles.gol}> <div>300+ {`  `} Git commits</div></div>
+                    </div>
+                    <div className = {styles.circlebtm}>
+                        <img alt = "gitcommits" src = {github}/>
+                    </div>
+                </div>
+            </div>
+        </Parallax>
+        <Parallax speed = {5}>
+            <div className = {styles.projects}>
+                <div className = {styles.newbtm}>
+                    <div className = {styles.newleft}>
+                        <div className = {styles.newhead}>About Me</div>
+                        <div className = {styles.newvice}>
+                             I like to think of myself as a designer/developer hybrid - a unicorn with excellent people skills.
+                        </div>
+                        <div className = {styles.newtext}>
+                            My non-tech roles and background have allowed me to develop exceptional soft skills and become a great communicator. I bring passion, determination and loyalty to any role I undertake.
+                        </div>
+                        <div className = {styles.newtext}>
+                            I just graduated from the Full Stack Developer Bootcamp from Masai School and I am now looking for my first full time development role with room to grow and learn. I find the creativity and visual aspects of Frontend very interesting and would love the opportunity to grasp more about UI/UX.
+                        </div>
+                        <div className = {styles.newextra}>
+                            I love coding, traveling, dancing (Fun fact: I'm a professional dancer!), sunny weekends and leaving everything better than I found it. Sometimes I bake.
+                        </div>
+                    </div>
+                    <div className = {styles.newright}>
+                        <img alt = "self" src = {self}/>
+                    </div>
+                </div>    
             </div>
         </Parallax>
         </>
